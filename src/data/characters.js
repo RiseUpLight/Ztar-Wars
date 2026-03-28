520eebb3601dee4b13b87c27aa8a8d4ac860353c
DS Schroeder <david.s.schroeder@gmail.com>
2026-03-27 19:13:09 -0500

    Add character data to characters.js

---
+++ src/data/characters.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: "Squeeps",
+    species: "Unknown",
+    affiliation: "Rebel",
+    description: "Agile sword-wielding medic"
+  },
+  {
+    name: "Ukob",
+    species: "?",
+    affiliation: "Rebel",
+    description: "Friendly clobbering buddy"
+  }
+]
