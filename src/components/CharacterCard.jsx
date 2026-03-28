9106424d7a563dae28be28638d6a776ef70a214b
DS Schroeder <david.s.schroeder@gmail.com>
2026-03-27 19:26:59 -0500

    Wrap CharacterCard content in a Link component

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
+
 export default function CharacterCard({ character }) {
   return (
-    <div className="card">
+    <Link to={`/character/${character.name}`} className="card">
       <h2>{character.name}</h2>
       <p><strong>Species:</strong> {character.species}</p>
       <p><strong>Affiliation:</strong> {character.affiliation}</p>
       <p>{character.description}</p>
-    </div>
+    </Link>
   )
 }
